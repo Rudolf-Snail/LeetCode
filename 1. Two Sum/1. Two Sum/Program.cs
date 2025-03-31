af3a6088372ebd2c8b92c1260af72da114acc1a7
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-03-31 16:42:17 +0200

    Two Sum, first correct solution.

---
+++ 1. Two Sum/1. Two Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _1._Two_Sum;
+
+Solution solution = new Solution();
+
+int[] nums = [2, 7, 11, 15];
+var target = 9;
+
+var result = solution.TwoSum(nums, target);
+
+foreach(int number in result)
+{
+    Console.Write($"{number} ");
+}    
---
+++ 1. Two Sum/1. Two Sum/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int index1 = 0, index2 = 0; 
+            int currentNumber1, currentNumber2;
+            int? result = null; 
+
+            for (index1 = 0; index1 < nums.Length; index1++)
+            {
+                currentNumber1 = nums[index1];
+
+                for (index2 = index1 + 1; index2 < nums.Length; index2++)
+                {
+                    currentNumber2 = nums[index2];
+
+                    result = currentNumber1 + currentNumber2;
+
+                    if (result == target)
+                        return [index1, index2];
+                }
+            }
+
+            return [-1, -1]; // Returned if no correct result is found.
+        }
+    }
+}
