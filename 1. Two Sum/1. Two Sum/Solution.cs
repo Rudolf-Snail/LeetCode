90877f3796317271918e085b6085e4ff9baa8785
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-03-31 22:37:37 +0200

    Two Sum, seventh solution. I used a dictionary this time; it's faster and it has type safety, likely due to the type safety, which means it can do the work during compilation time instead of run-time.

--- 1. Two Sum/1. Two Sum/Solution.cs
+++ 1. Two Sum/1. Two Sum/Solution.cs
@@ -10,7 +10,7 @@ namespace _1._Two_Sum
             int index1 = 0, index2 = 0;
             int currentNumber1, wantedNumber;
 
-            Hashtable hashtable = new Hashtable(length);
+            Dictionary<int, int> hashMap = new Dictionary<int, int>(length);
 
             for (index1 = 0; index1 < length; index1++)
             {
@@ -18,16 +18,14 @@ namespace _1._Two_Sum
 
                 wantedNumber = target - currentNumber1;
 
-                if (hashtable.ContainsKey(wantedNumber))
+                if (hashMap.ContainsKey(wantedNumber))
                 {
-#pragma warning disable CS8605 // Unboxing a possibly null value.
-                    index2 = ( int ) hashtable[wantedNumber];
-#pragma warning restore CS8605 // Unboxing a possibly null value.
+                    index2 = hashMap[wantedNumber];
                     
                     return [index1, index2];
                 }
 
-                hashtable[currentNumber1] = index1;
+                hashMap[currentNumber1] = index1;
             }
 
             return Array.Empty<int>(); 
