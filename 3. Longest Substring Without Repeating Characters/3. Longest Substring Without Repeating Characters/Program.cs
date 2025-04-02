e8a030a6946725ccdfc1946636e5a7a8a1a93a20
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-04-02 22:09:01 +0200

    Longest Substring Without Repeating Characters, first correct solution. Seems I took a very naive and inefficient approach, since it's quite slow and most people have a way better score. I wonder how I can improve it.

---
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _3._Longest_Substring_Without_Repeating_Characters;
+
+string s = "ohvhjdml";
+
+Solution solution = new();
+Console.WriteLine(solution.LengthOfLongestSubstring(s));
---
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace _3._Longest_Substring_Without_Repeating_Characters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int inputLength = s.Length;
+            Dictionary<char, int> letters = new(inputLength);
+
+            string longestSubstring = "";
+            string currentSubstring = "";
+            char currentCharacter;
+
+            for (int index = 0; index < inputLength; index++)
+            {
+                currentCharacter = s[index];
+
+                if (!letters.ContainsKey(currentCharacter))
+                {
+                    currentSubstring += currentCharacter;
+                    letters.Add(currentCharacter, currentSubstring.Length - 1);
+
+                    continue;
+                }
+                else
+                {
+                    if (currentSubstring.Length > longestSubstring.Length)
+                        longestSubstring = currentSubstring;
+
+                    currentSubstring = (letters[currentCharacter] == currentSubstring.Length - 1) ?
+                                        currentCharacter.ToString() : 
+                                        currentSubstring.Substring(letters[currentCharacter] + 1) + currentCharacter;
+
+                    letters.Clear();
+
+                    for (int substringIndex = 0; substringIndex < currentSubstring.Length; substringIndex++)
+                        letters.Add(currentSubstring[substringIndex], substringIndex);
+                }
+            }
+
+            longestSubstring = (longestSubstring.Length > currentSubstring.Length) ? longestSubstring : currentSubstring;
+
+            return longestSubstring.Length;
+        }
+    }
+}
