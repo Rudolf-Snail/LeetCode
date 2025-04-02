2d83c0df6abca91753f92d2f8186e850d1be7b44
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-04-02 16:53:09 +0200

    Add Two Numbers, second correct solution. I refactored it to be more readable and have less (mostly) unneeded complexity, but it's somewhat less performant.

--- 2. Add Two Numbers/2. Add Two Numbers/Solution.cs
+++ 2. Add Two Numbers/2. Add Two Numbers/Solution.cs
@@ -4,67 +4,66 @@
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            int addition = 0; 
-            int currentSum = l1.val + l2.val + addition; // You are given two NON-EMPTY linked-lists
-            addition = currentSum / 10;
+            int addition = 0, currentSum = 0;
+            int l1Value = l1.val;
+            int l2Value = l2.val;
+            ListNode currentNodeOfResult = new();
 
-            ListNode currentNumberResult = new(currentSum % 10); 
-            ListNode result = currentNumberResult;
+            CalculateCurrentNumber(ref addition, ref currentSum, currentNodeOfResult, ref l1Value, ref l2Value);
 
-            int l1Value, l2Value;
+            ListNode result = currentNodeOfResult;
 
-            for (ListNode currentL1Node = l1.next, currentL2Node = l2.next; currentL1Node != null || currentL2Node != null;)
+            for (ListNode? currentL1Node = l1.next, currentL2Node = l2.next; currentL1Node != null || currentL2Node != null || addition > 0;)
             {
-                currentNumberResult.next = new();
-                currentNumberResult = currentNumberResult.next;
+                currentNodeOfResult = CreateAndMoveToNextNode(currentNodeOfResult);
 
-                if (currentL1Node != null && currentL2Node != null)
-                {
-                    l1Value = currentL1Node.val;
-                    l2Value = currentL2Node.val;
-                
-                    currentSum = l1Value + l2Value + addition;
-                    addition = currentSum / 10;
-                    currentNumberResult.val = currentSum % 10;
+                CurrentNodesNumbers(out l1Value, out l2Value, currentL1Node, currentL2Node);
+                currentSum = CalculateCurrentNumber(ref addition, ref currentSum, currentNodeOfResult, ref l1Value, ref l2Value);
 
-                    currentL1Node = currentL1Node.next;
-                    currentL2Node = currentL2Node.next;
-                }
-                else if (currentL1Node == null)
-                {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                    l2Value = currentL2Node.val; // Once again, the compiler is wrong, or at least should be.
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-                    
-                    currentSum = l2Value + addition;
-                    addition = currentSum / 10;
-                    currentNumberResult.val = currentSum % 10;
-
-                    currentL2Node = currentL2Node.next;
-                }
-                else
-                {
-                    l1Value = currentL1Node.val;
-                    
-                    currentSum = l1Value + addition;
-                    addition = currentSum / 10;
-                    currentNumberResult.val = currentSum % 10;
-
-                    currentL1Node = currentL1Node.next;
-                }
-            }
-
-            while (addition > 0)
-            {
-                currentNumberResult.next = new();
-                currentNumberResult = currentNumberResult.next;
-                
-                currentSum = addition;
-                addition = currentSum / 10;
-                currentNumberResult.val = currentSum % 10;
+                NextNodes(ref currentL1Node, ref currentL2Node);
             }
 
             return result;
+        }
+
+        private static void NextNodes(ref ListNode? currentL1Node, ref ListNode? currentL2Node)
+        {
+            currentL1Node = NextNode(currentL1Node);
+            currentL2Node = NextNode(currentL2Node);
+        }
+
+        private static ListNode? NextNode(ListNode? currentNode)
+        {
+            if (currentNode == null)
+                return currentNode;
+
+            currentNode = currentNode.next;
+            return currentNode;
+        }
+
+        private static void CurrentNodesNumbers(out int l1Value, out int l2Value, ListNode? currentL1Node, ListNode? currentL2Node)
+        {
+            l1Value = CurrentNumber(currentL1Node);
+            l2Value = CurrentNumber(currentL2Node);
+        }
+
+        private static int CurrentNumber(ListNode? currentNode) => (currentNode != null) ? currentNode.val : 0;
+
+        private static int CalculateCurrentNumber(ref int addition, ref int currentSum, ListNode currentNodeOfResult, ref int l1Value, ref int l2Value)
+        {
+            currentSum = l1Value + l2Value + addition;
+            addition = currentSum / 10;
+            currentNodeOfResult.val = currentSum % 10;
+
+            return currentSum;
+        }
+
+        private static ListNode CreateAndMoveToNextNode(ListNode currentNumberResult)
+        {
+            currentNumberResult.next = new();
+            currentNumberResult = currentNumberResult.next;
+
+            return currentNumberResult;
         }
     }
 }
