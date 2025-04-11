518f1f6873856462ecca8dce0efbb85cbdfb28bc
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-04-11 18:57:31 +0200

    Median of Two Sorted Arrays, second correct solution. This time I used the proper algorithm, by reading about it on Leetcode.

--- 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays/Program.cs
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _4._Median_of_Two_Sorted_Arrays;
 
-int[] nums1 = [1, 2];
-int[] nums2 = [3, 4];
+int[] nums1 = [1, 3];
+int[] nums2 = [2];
 
 Solution solution = new();
 Console.WriteLine(solution.FindMedianSortedArrays(nums1, nums2));
--- 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays/Solution.cs
+++ 4. Median of Two Sorted Arrays/4. Median of Two Sorted Arrays/Solution.cs
@@ -2,71 +2,50 @@
 {
     public class Solution
     {
-        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        public double FindMedianSortedArrays(int[] A, int[] B)
         {
-            int length1 = nums1.Length, length2 = nums2.Length, combinedLength = length1 + length2;
-            bool isEven = combinedLength % 2 == 0;
-            int medianNumberIndex = isEven ? (combinedLength / 2) - 1 : combinedLength / 2;
+            int aLength = A.Length;
+            int bLength = B.Length;
 
-            int index = 0, index1 = 0, index2 = 0;
-            int currentNumber = int.MinValue, currentNumber1 = int.MinValue, currentNumber2 = int.MinValue;
-
-            for (; index <= medianNumberIndex; index++)
-                currentNumber = GetCurrentLowestNumber(nums1, nums2, length1, length2, ref index1, ref index2, 
-                                                       currentNumber, ref currentNumber1, ref currentNumber2);
-
-            double median = currentNumber;
-
-            if (isEven)
-            {
-                int secondNumber = GetCurrentLowestNumber(nums1, nums2, length1, length2, ref index1, ref index2, 
-                                                          currentNumber, ref currentNumber1, ref currentNumber2);
-                median = (currentNumber + secondNumber) / ( double ) 2;
-            }
-
-            return median;
+            return (aLength + bLength) % 2 == 1
+                ?    NthSmallestElement((aLength + bLength) / 2, A, B)
+                : (NthSmallestElement((aLength + bLength) / 2, A, B) + NthSmallestElement(((aLength + bLength) / 2) - 1, A, B)) / ( double ) 2;
         }
 
-        private static int GetCurrentLowestNumber(int[] nums1, int[] nums2, int length1, int length2, ref int index1, ref int index2, int currentNumber, ref int currentNumber1, ref int currentNumber2)
+        public static int NthSmallestElement(int n, int[] A, int[] B)
         {
-            if (index1 < length1 && index2 < length2)
-            {
-                FindCurrentLowestNumber(nums1, nums2, ref index1, ref index2, out currentNumber, out currentNumber1, out currentNumber2);
-            }
-            else if (index1 < length1)
-            {
-                ProcessLowestNumber(nums1, ref index1, out currentNumber, out currentNumber1);
-            }
-            else
-            {
-                ProcessLowestNumber(nums2, ref index2, out currentNumber, out currentNumber2);
-            }
+            int aStart = 0, aEnd = A.Length - 1;
+            int bStart = 0, bEnd = B.Length - 1;
 
-            return currentNumber;
-        }
+            int aIndex, aValue, bIndex, bValue;
 
-        private static void FindCurrentLowestNumber(int[] nums1, int[] nums2, ref int index1, ref int index2, out int currentNumber, out int currentNumber1, out int currentNumber2)
-        {
-            currentNumber1 = nums1[index1];
-            currentNumber2 = nums2[index2];
-
-            if (currentNumber1 <= currentNumber2)
+            while (true)
             {
-                currentNumber = currentNumber1;
-                index1++;
-            }
-            else
-            {
-                currentNumber = currentNumber2;
-                index2++;
-            }
-        }
+                if (aEnd < aStart)
+                    return B[n - aStart];
+                if (bEnd < bStart)
+                    return A[n - bStart];
 
-        private static void ProcessLowestNumber(int[] nums, ref int index, out int currentNumber, out int currentArrayNumber)
-        {
-            currentArrayNumber = nums[index];
-            currentNumber = currentArrayNumber;
-            index++;
+                aIndex = (aStart + aEnd) / 2;
+                bIndex = (bStart + bEnd) / 2;
+                aValue = A[aIndex];
+                bValue = B[bIndex];
+
+                if (aIndex + bIndex < n)
+                {
+                    if (aValue < bValue)
+                        aStart = aIndex + 1;
+                    else
+                        bStart = bIndex + 1;
+                }
+                else
+                {
+                    if (bValue < aValue)
+                        aEnd = aIndex - 1;
+                    else
+                        bEnd = bIndex - 1;
+                }
+            }
         }
     }
 }
