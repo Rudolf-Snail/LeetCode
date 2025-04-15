8f93cdd7fad1a5c62e9954e2d376564e5b3500a0
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-04-15 12:04:43 +0200

    Longest Palindromic Substring, second correct solution. Based on the second method mentioned by LeetCode.

--- 5. Longest Palindromic Substring/5. Longest Palindromic Substring/Solution.cs
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring/Solution.cs
@@ -4,35 +4,44 @@
     {
         public string LongestPalindrome(string input)
         {
-            for (int length = input.Length; length >= 1 ; length--)
+            int n = input.Length;
+            bool[,] dp = new bool[n, n];
+            int[] answer = [0, 0];
+
+            int i;
+
+            for (i = 0; i < n; i++)
+                dp[i, i] = true;
+
+            int j;
+
+            for (i = 0, j = 1; j < n; i++, j++)
             {
-                for (int start = 0; start <= input.Length - length; start++)
+                if (input[i] == input[j])
                 {
-                    if (IsPalindrome(start, start + length, input))
-                        return input.Substring(start, length);
+                    dp[i, j] = true;
+                    answer = [i, j];
                 }
             }
 
-            return "";
-        }
-
-        public bool IsPalindrome(int startIndex, int endIndex, string text)
-        {
-            if (text == null)
-                return false;
-            else if (startIndex == endIndex)
-                return true;
-
-            int left = startIndex;
-            int right = endIndex - 1; 
-
-            for (; left < right; left++, right--)
+            for (int difference = 2; difference < n; difference++)
             {
-                if (text[left] != text[right])
-                    return false;
+                for (i = 0; i < n - difference; i++)
+                {
+                    j = i + difference;
+
+                    if (input[i] == input[j] && dp[i + 1, j - 1])
+                    {
+                        dp[i, j] = true;
+                        answer = [i, j];
+                    }
+                }
             }
 
-            return true;
+            i = answer[0];
+            j = answer[1];
+
+            return input[i..(j + 1)];
         }
     }
 }
