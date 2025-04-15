033b3c8e84e52be3cd01724ebd24cfa1a809012e
Rudolf-Snail <81647773+Rudolf-Snail@users.noreply.github.com>
2025-04-15 10:01:24 +0200

    Longest Palindromic Substring, first correct solution. I couldn't do this myself, so I did it by reading the LeetCode solution (although I may have done it and not known about it, because I didn't print out the result at first).

---
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _5._Longest_Palindromic_Substring;
+
+string input = "babad";
+Solution solution = new();
+
+Console.WriteLine(solution.LongestPalindrome(input));
---
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace _5._Longest_Palindromic_Substring
+{
+    public class Solution
+    {
+        public string LongestPalindrome(string input)
+        {
+            for (int length = input.Length; length >= 1 ; length--)
+            {
+                for (int start = 0; start <= input.Length - length; start++)
+                {
+                    if (IsPalindrome(start, start + length, input))
+                        return input.Substring(start, length);
+                }
+            }
+
+            return "";
+        }
+
+        public bool IsPalindrome(int startIndex, int endIndex, string text)
+        {
+            if (text == null)
+                return false;
+            else if (startIndex == endIndex)
+                return true;
+
+            int left = startIndex;
+            int right = endIndex - 1; 
+
+            for (; left < right; left++, right--)
+            {
+                if (text[left] != text[right])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
